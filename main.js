8bb568cfc7eba8027499b1a53a148faa059787a7
Dhanfia <dhanfiarahman@gmail.com>
2020-04-22 14:09:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+let isDrawing = false;
+
+const clearButton = document.querySelector('.clear-button');
+const downloadButton = document.querySelector('.download-button');
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stopDrawing);
+
+clearButton.addEventListener('click', clearCanvas);
+downloadButton.addEventListener('click', downloadAsImage);
+window.addEventListener('resize', resizeCanvas);
+
+function startDrawing(e) {
+    isDrawing = true;
+    drawInMobile(e);
+}
+
+function stopDrawing() {
+    isDrawing = false;
+    ctx.beginPath();
+}
+
+function draw({ clientX: x, clientY: y }) {
+    if (!isDrawing) {
+        return;
+    } 
+    ctx.lineWidth = 8;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#171717";
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function downloadAsImage() {
+    var dataURL = canvas.toDataURL('image/png');
+    downloadButton.href = dataURL;
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
